e833a8d34a59b840a9226feef4093187d934a641
PHRUEK APIROMRAT <63010650@KMITL.AC.TH>
2020-08-29 18:10:16 +0700

    sum

---
+++ assignment08/Source.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int input, i, j = 0;
+	scanf_s("%d", &input);
+	while (input > 10)
+	{
+		i = input;
+		while (i > 10)
+		{
+			j += i % 10;
+			i = (i - (i % 10)) / 10;
+		}
+		j += i;
+		printf("%d\n", j);
+		input = j;
+		j = 0;
+	}
+	return 0;
+}
